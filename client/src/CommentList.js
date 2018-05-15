0905776a2f86b812fac2e09c5419c4b2c5cde438
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-05-15 19:09:14 +0900

    add backend

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import Comment from './Comment';
 
 const CommentList = (props) => {
-  const commentNodes = props.data.map(comment => (
-    <Comment author={comment.author} key={comment._id} id={comment._id}>
-      { comment.text}
-    </Comment>
-  ));
-  return (
-    <div>
-      { commentNodes }
-    </div>
-  );
+	const commentNodes = props.data.map((comment) => (
+		<Comment
+			author={comment.author}
+			key={comment._id}
+			id={comment._id}
+			timestamp={comment.updatedAt}>
+			{comment.text}
+		</Comment>
+	));
+	return <div>{commentNodes}</div>;
 };
 
 // CommentList.propTypes = {
